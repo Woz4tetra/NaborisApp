5c410ceeaa26de3e1cff54e9df17d93170c9f079
Woz4tetra <woz4tetra@gmail.com>
2017-11-26 09:30:57 -0500

    Adding config files placed in /etc/apache2/sites-available

--- NaborisApp/__init__.py
+++ NaborisApp/__init__.py
@@ -1,106 +1,143 @@
-from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask import Flask, jsonify, abort, make_response, request, url_for, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth()
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
+ROBOT_USER = "robot"
 
 users = {
-    "susan": "bye",
-    "woz": "something"
+    "user": generate_password_hash("something")
+    ROBOT_USER: generate_password_hash("naboris")
 }
 
-@auth.get_password
-def get_pw(username):
+@auth.verify_password
+def verify_password(username, password):
     if username in users:
-        return users.get(username)
-    return None
+        return check_password_hash(users.get(username), password)
+    return False
+
 
 @auth.error_handler
 def unauthorized():
     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
-        else:
-            new_task[field] = task[field]
-    return new_task
-
 @app.route('/')
 @auth.login_required
-def index():
-    return "Hello, %s!" % auth.username()
+def index_page():
+    """Page for viewing the camera feed and sending commands to the robot"""
+    return render_template('index.html')
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+
+@app.route('/status')
 @auth.login_required
-def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+def status_page():
+    """Page for checking the general status of the robot"""
+    return render_template('status-page.html')
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+@app.route('/cmd', methods=['PUT'])
+@auth.login_required
+def put_command():
+    """Put a command to send to the robot"""
+    return jsonify()
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
-def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify({'result': True})
+@app.route('/api', methods=['GET'])
+@auth.login_required
+def get_api():
+    """List all the properties and functions of the api"""
+    return jsonify()
+
+
+@app.route('/api/robot/status', methods=['PUT'])
+@auth.login_required
+def set_status():
+    """Function for the robot. Update the robot's current status"""
+    if auth.username() == ROBOT_USER:
+        return jsonify()
+
+
+def frame_generator(camera):
+    """Camera frame generator"""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/api/robot/rightcam', methods=['GET'])
+@auth.login_required
+def get_camera():
+    """Stream frames from the right camera to the client"""
+    return Response(frame_generator(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/api/robot/imu', methods=['PUT'])
+@auth.login_required
+def put_imu():
+    """Function for the robot. Update the IMU's data on the server"""
+    if auth.username() == ROBOT_USER:
+        return jsonify()
+
+
+@app.route('/api/robot/trajectory', methods=['PUT'])
+@auth.login_required
+def put_trajectory():
+    """Function for the robot. Update the trajectory on the server according to the current algorithm in use"""
+    if auth.username() == ROBOT_USER:
+        return jsonify()
+
+
+@app.route('/api/robot/sound', methods=['POST'])
+@auth.login_required
+def post_sound():
+    """Function for the robot. Update the list of sounds"""
+    if auth.username() == ROBOT_USER:
+        return jsonify()
+
+
+@app.route('/api/robot/sound', methods=['PUT'])
+@auth.login_required
+def put_sound():
+    """Function for the robot. Update the list of sounds"""
+    if auth.username() == ROBOT_USER:
+        return jsonify()
+
+
+@app.route('/api/robot/move_command', methods=['POST'])
+@auth.login_required
+def post_move_command():
+    """Function for the robot. Update the list of move commands"""
+    if auth.username() == ROBOT_USER:
+        return jsonify()
+
+
+@app.route('/api/robot/move_command', methods=['PUT'])
+@auth.login_required
+def put_move_command():
+    """Function for the robot. Update the list of move commands"""
+    if auth.username() == ROBOT_USER:
+        return jsonify()
+
+
+@app.route('/api/robot/look', methods=['POST'])
+@auth.login_required
+def post_turret():
+    """Function for the robot. Update the list of looking commands"""
+    return jsonify()
+
+@app.route('/api/robot/look', methods=['PUT'])
+@auth.login_required
+def put_turret():
+    """Function for the robot. Update the list of looking commands"""
+    return jsonify()
+
 
 @app.errorhandler(404)
+@auth.login_required
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
